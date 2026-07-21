0fb83ccd7703faa5a15cb225c6cbff0579a14659
sprinttops00 <sprinttops@gmail.com>
2026-07-21 01:22:21 -0400

    Update 01 Crash.jsx

--- src/pages/Crash.jsx
+++ src/pages/Crash.jsx
@@ -93,7 +93,7 @@ export default function Crash() {
 
     await statsDB.update(currentPlayer.id, {
       total_games_played: (currentPlayer.user_statistics?.total_games_played || 0) + 1,
-      total_wins: (player.user_statistics?.total_wins || 0) + (won ? 1 : 0),
+      total_wins: (currentPlayer.user_statistics?.total_wins || 0) + (won ? 1 : 0),
       total_losses: (currentPlayer.user_statistics?.total_losses || 0) + (won ? 0 : betAmount),
       biggest_win: Math.max(currentPlayer.user_statistics?.biggest_win || 0, won ? payout : 0),
       current_streak: newStreak,
